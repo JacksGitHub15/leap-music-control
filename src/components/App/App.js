01673457de97ccece018e04e8697b24f228ae722
Jack Kearney <jackkearney15@gmail.com>
2020-11-23 10:49:15 +0000

    First commit, setting up menu

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Menu from '../Menu/Menu.js';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Menu />
+      </header>
+    </div>
+  );
+}
+
+export default App;
