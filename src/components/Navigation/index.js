29995be49b1003b4995b61b7fa59bf4bbbb824e0
Jack Kearney <jackkearney15@gmail.com>
2020-11-30 10:21:15 +0000

    Fixing button layout

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -5,14 +5,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Navigation = () => (
     <header bg="primary">
-      <h1 className="h3 mb-3 font-weight-normal, text-center">Leap Music Control</h1>
-      <div>
-        <Link to={ROUTES.AUDIO_STUDIO}>Audio Studio</Link>
-        <Link to={ROUTES.MUSIC_VISUALIZER}>Music Visualizer</Link>
-        <Link to={ROUTES.ABOUT}>About</Link>
-        <Link to={ROUTES.EXIT}>Exit</Link>
-      </div>
+      <h1 className="h1 mb-3 font-weight-normal, text-center">Leap Music Control</h1>
+      <div><Link to={ROUTES.AUDIO_STUDIO}>Audio Studio</Link></div>
+      <div><Link to={ROUTES.MUSIC_VISUALIZER}>Music Visualizer</Link></div>
+      <div><Link to={ROUTES.ABOUT}>About</Link></div>
+      <div><Link to={ROUTES.EXIT}>Exit</Link></div>
     </header>
   );
 
-  export default Navigation;
+export default Navigation;
