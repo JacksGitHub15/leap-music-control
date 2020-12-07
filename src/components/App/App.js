f76ed15695715653a171d508ca7b5c9169f4bf65
Jack Kearney <jackkearney15@gmail.com>
2020-12-07 22:08:30 +0000

    Was broken, now fine, leap motion working is next

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,18 +9,21 @@ import MusicVisualizer from '../MusicVisualizer';
 import About from '../About';
 import Exit from '../Exit';
 import * as ROUTES from '../../constants/routes.js';
+import { LeapProvider } from '../../Leap/leap.js'
 import './App.css';
 
 const App = () => (
   <Router>
     <div className="App">
-      <Navigation />
-      <div className="ButtonLayout">
-        <Route path={ROUTES.AUDIO_STUDIO} component={AudioStudio} />
-        <Route path={ROUTES.MUSIC_VISUALIZER} component={MusicVisualizer} />
-        <Route path={ROUTES.ABOUT} component={About} />
-        <Route path={ROUTES.EXIT} component={Exit} />
-      </div>
+      <LeapProvider>
+        <Navigation />
+        <div className="ButtonLayout">
+          <Route path={ROUTES.AUDIO_STUDIO} component={AudioStudio} />
+          <Route path={ROUTES.MUSIC_VISUALIZER} component={MusicVisualizer} />
+          <Route path={ROUTES.ABOUT} component={About} />
+          <Route path={ROUTES.EXIT} component={Exit} />
+        </div>
+      </LeapProvider>
     </div>
   </Router>
 );
